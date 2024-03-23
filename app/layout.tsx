910b6154814e5cc1248f93f936b2f0af22c88019
Hamil <dimapanat.hamil@gmail.com>
2024-03-22 20:07:30 -0400

    google analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
@@ -43,6 +44,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+		<GoogleAnalytics gaId="G-R3SRH3J9Z5"/>
       </body>
     </html>
   );
