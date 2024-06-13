1d741a2eaeb07852098e0e56db9edb2a336106e6
Hamil <dimapanat.hamil@gmail.com>
2024-06-13 10:58:08 -0400

    new resume

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -39,7 +39,7 @@ export default function HeroSection() {
           <div className="glow absolute top-[40%] right-1/2 -z-10"></div>
         </div>
         <div className="absolute bottom-5 sm:bottom-14 left-0 sm:left-10">
-          <a target="_blank" href="/hamil-dimapanat-resume.pdf">
+          <a target="_blank" href="/Hamil Dimapanat.pdf">
             <MovingBorderBtn
               borderRadius="0.5rem"
               className="p-3 font-semibold"
